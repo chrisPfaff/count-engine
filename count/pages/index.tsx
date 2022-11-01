88240a7cbc4334423c402cb9699ac24b8aa0d1eb
Chris Pfaff <chrispfaff10@gmail.com>
2022-11-01 09:53:11 -0500

    added next

---
+++ count/pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>10000 Hours</title>
+        <meta name="description" content="Countdown" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}></main>
+
+      <footer className={styles.footer}></footer>
+    </div>
+  );
+}
