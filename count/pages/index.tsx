0548b1961571a014d8311c38752fd478efbf3515
Chris Pfaff <chrispfaff10@gmail.com>
2022-11-01 11:53:06 -0500

    added dynamoDB

--- count/pages/index.tsx
+++ count/pages/index.tsx
@@ -1,18 +1,25 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import useSWR from "swr";
+
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>10000 Hours</title>
-        <meta name="description" content="Countdown" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+  const { data, error } = useSWR("/api/getDB?id=10000", fetcher);
 
-      <main className={styles.main}></main>
+  if (error) return "An error has occurred.";
+  if (!data) return "Loading...";
+  return <code>{JSON.stringify(data, null, 2)}</code>;
 
-      <footer className={styles.footer}></footer>
-    </div>
-  );
+  // return (
+  //   <div className={styles.container}>
+  //     <Head>
+  //       <title>10000 Hours</title>
+  //       <meta name="description" content="Countdown" />
+  //       <link rel="icon" href="/favicon.ico" />
+  //     </Head>
+
+  //     <footer className={styles.footer}></footer>
+  //   </div>
+  // );
 }
